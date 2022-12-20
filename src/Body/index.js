00202392628d44db4ac3219c24773071a0a48b82
rmehtije <rmehtije@gmail.com>
2022-12-20 15:18:06 +0200

    homework searchBar

--- src/Body/index.js
+++ src/Body/index.js
@@ -1,26 +1,43 @@
+import { useState } from 'react';
 import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
+import Button from 'react-bootstrap/Button';
 import Map from './Map';
 import TimeSelector from './TimeSelector';
 import './body.scss';
+import SearchBar from './SearchBar';
 
 function Body() {
+
+    const [showBar, setShowBar] = useState(false);
+
+    const handleCloseBar = () => setShowBar(false);
+    const handleShowBar = () => setShowBar(true);
+
     return (
-        <Tabs
-            defaultActiveKey="now"
-            id="justify-tab-example"
-            className="mb-3"
-            justify
-        >
-            <Tab eventKey="now" title="Now">
-                <TimeSelector id="now"/>
-                <Map />
-            </Tab>
-            <Tab eventKey="forecast" title="Forecast">
-                <TimeSelector id="forcast"/>
-                <Map />
-            </Tab>
-        </Tabs>
+        <>
+            <Button variant="primary" onClick={handleShowBar}>
+                Launch
+            </Button>
+            
+            <SearchBar show={showBar} handleClose={handleCloseBar} />
+            
+            <Tabs
+                defaultActiveKey="now"
+                id="justify-tab-example"
+                className="mb-3"
+                justify
+            >
+                <Tab eventKey="now" title="Now">
+                    <TimeSelector id="now" />
+                    <Map />
+                </Tab>
+                <Tab eventKey="forecast" title="Forecast">
+                    <TimeSelector id="forcast" />
+                    <Map />
+                </Tab>
+            </Tabs>
+        </>
     );
 }
 
